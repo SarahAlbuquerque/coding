cd01c9a07ef0efa34c95756e9fc7bf1ffb996143
Sarah Albuquerque <sarah.albuquerque@ee.ufcg.edu.br>
2018-01-22 15:10:26 -0300

    question 2041 from Urionline: EOFError: EOF when reading a line

---
+++ 2041.py
@@ -0,0 +1,30 @@
+
+def get_nextSequence(s):
+
+    s.append('0') # marcando o fim da string
+    string = []
+    cont = 1
+    for i in range(len(s)-1):
+        if s[i]==s[i+1]:
+            cont = cont + 1
+        else:
+            string.append(cont)
+            string.append(s[i])
+            cont = 1
+    nextSequence = ''.join(map(str, string))
+    return nextSequence
+
+N = -1
+while(True):
+    N = int(input())
+    sequence = '3'
+
+    for j in range(N-1):
+        if N == 1:
+            print(sequence)
+        else: 
+            aux = []
+            aux = get_nextSequence(list(sequence))
+            sequence = aux[:]
+    print(sequence)
+    
