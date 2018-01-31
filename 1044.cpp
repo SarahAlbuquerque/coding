13676ccba74aafbcfff383dad1fa0db1426a6297
Sarah Albuquerque <sarah.albuquerque@ee.ufcg.edu.br>
2018-01-31 15:16:06 -0300

    add c++ code

---
+++ 1044.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  
+  int A, B;
+  
+  cin >> A >> B;
+  
+  if(A==0 or B==0)
+  {
+    cout << "Sao Multiplos" << endl;
+  }
+  else 
+  {
+    if(A%B == 0 || B%A == 0)
+    {
+      cout << "Sao Multiplos" << endl;
+    }
+    else
+    {
+      cout << "Nao sao Multiplos" << endl;
+    }
+  }
+    
+}
